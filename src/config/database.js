9d506798d2cf8d63973514e105afc892ae676bd7
Humberto Bernal Mondragon <55298515+bemh1997@users.noreply.github.com>
2025-06-05 22:41:57 -0600

    Update database.js

--- src/config/database.js
+++ src/config/database.js
@@ -4,15 +4,15 @@ const path = require('path');
 
 const { DB_USER, DB_PASSWORD, DB_HOST, DB_NAME } = require('./env');
 
-const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`, {
-  logging: false,
-  native: false,
-});
+//const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`, {
+//  logging: false,
+//  native: false,
+//});
 
-// const sequelize = new Sequelize(process.env.DB_DEPLOY, {
-//   dialect: 'postgres',
-//   logging: false, // o true si quieres ver queries en consola
-// });
+const sequelize = new Sequelize(process.env.DB_DEPLOY, {
+   dialect: 'postgres',
+   logging: false, // o true si quieres ver queries en consola
+});
 
 const modelDefiners = [];
 
