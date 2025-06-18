dbb469ca0757da02853234fbd4a2abaef9365c2f
Humberto Bernal <bemh.1997@gmail.com>
2025-06-17 18:16:30 -0600

    'switch'

--- src/models/usuarios.model.js
+++ src/models/usuarios.model.js
@@ -3,9 +3,9 @@ const { DataTypes } = require('sequelize');
 module.exports = (sequelize) => {
   sequelize.define('Usuario', {
   id_usuario: {
-    type: DataTypes.BIGINT,
-    primaryKey: true,
-    autoIncrement: true
+    type: DataTypes.UUID,
+    defaultValue: DataTypes.UUIDV4,
+    primaryKey: true
   },
   nombre: {
     type: DataTypes.TEXT,
