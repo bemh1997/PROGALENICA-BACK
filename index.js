7c2ffaf8442c420c6c90e6fbad2c0d1bcbe8f996
Humberto Bernal <bemh.1997@gmail.com>
2025-07-25 04:10:02 -0600

    Cambios en productos y en config para su procesamiento en aws

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ require('dotenv').config
 const server = require('./src/app.js');
 const { conn } = require('./src/config/database.js');
 const port = process.env.PORT || 3001;
-conn.sync({ force: true }).then(() => {
-  server.listen(port, () => {
+conn.sync({ alter: true }).then(() => {
+  server.listen(port, '0.0.0.0', () => {
     console.log('My API listening at', port);
   });
 });
