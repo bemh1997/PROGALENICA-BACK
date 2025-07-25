4a9801b33bed839235a0b98e558bced507e8be65
Humberto Bernal <bemh.1997@gmail.com>
2025-07-25 13:21:45 -0600

    hotfix

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ require('dotenv').config
 const server = require('./src/app.js');
 const { conn } = require('./src/config/database.js');
 const port = process.env.PORT || 3001;
-conn.sync({ force: true }).then(() => {
+conn.sync({ alter: true }).then(() => {
   server.listen(port, '0.0.0.0', () => {
     console.log('My API listening at', port);
   });
